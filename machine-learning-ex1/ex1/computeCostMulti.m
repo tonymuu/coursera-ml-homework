fe621fd8a4bdcd33c02e958588f7d66c14f82a80
tonymu <tony.mu101999@gmail.com>
2019-03-10 17:53:07 -0400

    homework assignment #1

---
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -0,0 +1,30 @@
+function J = computeCostMulti(X, y, theta)
+%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
+%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+h = zeros(m, 1)
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta
+%               You should set J to the cost.
+
+
+for i = 1:m
+    
+    h(i, 1) = X(i, :) * theta;
+
+diff = h - y;
+
+J = (diff' * diff) / (2 * m);
+
+
+
+% =========================================================================
+
+end
