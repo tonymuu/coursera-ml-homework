983f614682b48a29c4bf11a265615496dc5d1cc9
tonymu <tony.mu101999@gmail.com>
2019-03-10 20:21:25 -0400

    homework #2: logistic regression and regularization; use of optimized fminunc

---
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -0,0 +1,29 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid function
+%   g = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = zeros(size(z));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+%               vector or scalar).
+
+for i = 1:size(g)(1)
+  for j = 1:size(g)(2)
+    g(i, j) = sigmoidSingle(z(i, j));
+  end
+end
+
+
+
+% =============================================================
+
+end
+
+
+function res = sigmoidSingle(n)
+
+res = 1 / (1 + e ^ -n);
+
+end
